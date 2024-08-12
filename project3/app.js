1145dca8047a015075d45397c40757cdf2a47327
OnuohaTech <davisonuoha2012@gmail.com>
2024-08-12 12:23:46 +0100

    Add files via upload

---
+++ project3/app.js
@@ -0,0 +1,8 @@
+const circle = document.querySelector('.circle')
+
+circle.addEventListener('mouseenter',() => {
+    circle.classList.add('hover')
+})
+circle.addEventListener('mouseleave',() => {
+    circle.classList.remove('hover')
+})
